dc6ccc37bac70af533da1d361be92cc6678ef1ab
official-techsupport <official_techsupport@protonmail.com>
2022-01-23 01:10:18 +0200

    refactor and fix (in the good sense) the slot game

--- files/classes/slots.py
+++ files/classes/slots.py
@@ -1,5 +1,6 @@
 from json.encoder import INFINITY
 import random
+from typing import NamedTuple
 from .comment import *
 from files.helpers.const import *
 
@@ -7,29 +8,30 @@ class Slots:
     commandWord = "!slots"
     minimumBet = 5
     maximumBet = INFINITY
-    symbols = {"♦️", "♠️", "♥️", "♣️", "⚧", "🔞", "⚛️", "☢️", "✡️", "⚔️", "🐱"}
 
-    # Common...
-    commonRatio = 4
-    commonPayout = 2
+    class Category(NamedTuple):
+        symbols: list
+        ratio: int
+        payout: int
 
-    # Uncommon.
-    uncommonIndex = 4
-    uncommonRatio = 3
-    uncommonPayout = 3
-
-    # Rare~
-    rareIndex = 8
-    rareRatio = 2
-    rarePayout = 5
-
-    # Jackpot!
-    jackpotIndex = 10
-    jackpotRatio = 1
-    jackpotPayout = 100
+    categories = [
+        Category("♦️ ♠️ ♥️ ♣️".split(), 4, 2),
+        Category("⚧ 🔞 ⚛️ ☢️".split(), 3, 3),
+        Category("✡️ ⚔️".split(), 2, 5),
+        Category("🐱".split(), 1, 100),
+    ]
 
     def __init__(self, g):
         self.db = g.db
+        # Generate full set of symbols.
+        symbols = []
+        payouts = {}
+        for c in self.categories:
+            for s in c.symbols:
+                symbols.extend([s] * c.ratio)
+                payouts[s] = c.payout
+        self.symbols = symbols
+        self.payouts = payouts
 
     # Check for !slots<wager>
     def check_for_slots_command(self, in_text, from_user, from_comment):
@@ -58,74 +60,22 @@ class Slots:
         else:
             return True
 
-    # Generate full set of symbols.
-    def count_out_symbols(self):
-        countedSymbols = []
-        payoutLookup = {}
-        index = 0
-
-        for item in self.symbols:
-            count = 0
-
-            if index == self.jackpotIndex:
-                count = self.jackpotRatio
-                payoutLookup[item] = self.jackpotPayout
-            elif index >= self.rareIndex:
-                count = self.rareRatio
-                payoutLookup[item] = self.rarePayout
-            elif index >= self.uncommonIndex:
-                count = self.uncommonRatio
-                payoutLookup[item] = self.uncommonPayout
-            else:
-                count = self.commonRatio
-                payoutLookup[item] = self.commonPayout
-
-            while count > 0:
-                countedSymbols.append(item)
-                count -= 1
-            
-            index += 1
-            
-        random.shuffle(countedSymbols)
-            
-        return { 'symbols': countedSymbols, 'payout': payoutLookup }
-
-    # Consolation prizes return the user's wager.
-    def check_for_consolation(self, symbols):
-        # 1. Any 2 matching.
-        if symbols[0] == symbols[1] or symbols[0] == symbols[2] or symbols[1] == symbols[2]:
-            return True
-        # 2. Any instance of jackpot.
-        for symbol in symbols:
-            if symbol == "🐱":
-                return True
-        
-        return False
-
     # Actually make the relevant calls
     def pull_the_arm(self, from_user, amount, from_comment):
-        # Charge user for the bet
-        self.charge_user(from_user, amount)
-
         # Determine the outcome
-        result1 = self.count_out_symbols()
-        result2 = self.count_out_symbols()
-        result3 = self.count_out_symbols()
-        symbol1 = result1['symbols'][0]
-        symbol2 = result2['symbols'][0]
-        symbol3 = result3['symbols'][0]
-        payout = result1['payout'][symbol1]
-        isMatch = symbol1 == symbol2 and symbol2 == symbol3
-        resultSymbols = [symbol1, symbol2, symbol3]
-        isConsolation = self.check_for_consolation(resultSymbols)
+        s1 = random.choice(self.symbols)
+        s2 = random.choice(self.symbols)
+        s3 = random.choice(self.symbols)
+        resultSymbols = [s1, s2, s3]
 
-        if isMatch:
+        if s1 == s2 == s3:
             # Pay out
-            reward = amount * payout
+            reward = amount * (self.payouts[s1] - 1) # withhold the bet
             self.credit_user(from_user, reward)
-        elif isConsolation:
-            # Refund wager
-            self.credit_user(from_user, amount)
+        elif s1 == s2 or s1 == s3 or s2 == s3 or "🐱" in resultSymbols:
+            pass # Refund wager - do nothing
+        else:
+            self.charge_user(from_user, amount)
 
         return "".join(resultSymbols)
 
